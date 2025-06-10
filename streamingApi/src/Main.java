3adc5e5f8d8ec08728e2890b071149aa27b6f773
Pardum <anonymosek@gmail.com>
2025-06-10 14:44:17 +0530

    streamapi program

--- streamingApi/src/Main.java
+++ streamingApi/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-            
-    }
+        System.out.println("Hello world");
+                List<Integer> numbers = Arrays.asList(5, 2, 8, 3, 1, 4, 6);
+
+                List<Integer> result = numbers.stream()
+                        .filter(n -> n % 2 == 0).collect(Collectors.toList());
+
+            }
 }
